e4cd1e803d436997c8d88e93c9ee835fec88dda2
葛跃雄 <geyuexiong@peilian.com>
2018-12-29 15:58:18 +0800

    初始化

---
+++ src/components/video.vue
@@ -0,0 +1,140 @@
+<template>
+        <div>
+            <video 
+            :src="videoSrc" 
+            :autoplay="autoplay"
+            :audioTracks="audioTracks"
+            @ended='ended()'
+            @error='error()'
+            @play='play()'
+            @pause='pause()'
+            @load='load()'
+            @canPlayType='canPlayType()'
+            @addTextTrack='addTextTrack()'
+            ></video>
+        </div>
+</template>
+
+<script>
+export default {
+    name:'',
+    props:{
+        videoSrc: {
+            type: String,
+            default: 'http://www.w3school.com.cn/i/movie.ogg'
+        },
+        autoplay: {
+            type: Boolean,
+            default: false
+            },
+        audioTracks: {
+            type:String,
+            default:''
+        },
+        buffered: {
+            type:Object,
+            default:''
+        },
+        controller: {
+            type:Object,
+            default:''
+        },
+        controls: {
+
+        },
+        crossOrigin: {
+
+        },
+        currentSrc: {
+
+        },
+        currentTime: {
+
+        },
+        defaultMuted: {
+
+        },
+        defaultPlaybackRate: {
+
+        },
+        duration: {
+
+        },
+        ended: {
+
+        },
+        error: {
+
+        },
+        height: {
+
+        },
+        loop: {
+
+        },
+        mediaGroup: {
+
+        },
+        muted: {
+            type: Boolean,
+            default: false
+        },
+        networkState: {
+
+        },
+        paused: {
+            type: Boolean,
+            default: false
+        },
+        playbackRate: {
+
+        },
+        played: {
+
+        },
+        poster: {
+
+        },
+        preload: {
+
+        },
+        readyState: {
+
+        },
+        startDate: {
+
+        },
+        seekable: {
+
+        },
+        seeking:{
+
+        },
+        textTracks: {
+
+        },
+        videoTracks: {
+
+        },
+        volume: {
+
+        },
+        width: {
+
+        }
+
+    }
+}
+</script>
+
+
+
+<style lang="less" scoped>
+
+</style>
+
+
+
+
+
+
